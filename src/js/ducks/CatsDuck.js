666a3ef6f152b29591c3ba73ffe5d448347cd5ad
Gaurav Kumar <gaurav@recruiterbox.com>
2017-11-04 00:25:16 +0530

    Add actions for setting selected category and all categories of cats

--- src/js/ducks/CatsDuck.js
+++ src/js/ducks/CatsDuck.js
@@ -1,22 +1,27 @@
 import {createAction} from 'redux-actions';
 import keyMirror from 'keymirror';
+import _ from 'underscore';
 import cats_data from '../../data/cats.json';
 
 const default_state = {
     cats: [],
-    category: null
+    categories: [],
+    selected_category: null
 };
 
 const Action_Types = keyMirror({
     'GET_CATS': null,
     'SET_CATS': null,
-    'SET_CAT_CATEGORY': null
+    'SET_CAT_CATEGORY': null,
+    'SET_CAT_CATEGORIES': null,
 });
 
 export const getCats = createAction(
     Action_Types.GET_CATS,
     (dispatch) => {
-        dispatch(setCats(dispatch, cats_data.cats));
+        const cats = cats_data.cats;
+        dispatch(setCats(dispatch, cats));
+        dispatch(setCatCategories(dispatch, cats));
     }
 );
 
@@ -29,7 +34,17 @@ export const setCats = createAction(
     }
 );
 
-export const setCatCategory = createAction(
+export const setCatCategories = createAction(
+    Action_Types.SET_CAT_CATEGORIES,
+    (dispatch, cats) => {
+        const categories = _.chain(cats).pluck('category').uniq().value();
+        return {
+            categories
+        }
+    }
+);
+
+export const setSelectedCatCategory = createAction(
     Action_Types.SET_CAT_CATEGORY,
     (dispatch, cats, category) => {
         const category_cats = cats.filter(cat => cat.category === category);
@@ -51,10 +66,16 @@ export default function reducer(state, action) {
                 cats: action.payload.cats
             };
 
+        case Action_Types.SET_CAT_CATEGORIES:
+            return {
+                ...state,
+                categories: action.payload.categories
+            };
+
         case Action_Types.SET_CAT_CATEGORY:
             return {
                 ...state,
-                category: action.payload.category
+                selected_category: action.payload.category
             };
 
         default:
