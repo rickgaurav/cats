f3948afd7d0b2f4f24eda110cc09484a635312bc
Gaurav Kumar <gaurav@recruiterbox.com>
2017-11-03 23:28:58 +0530

    Add category to cats data and Add action to filter cat data to set category

--- src/js/containers/CatsListContainer.js
+++ src/js/containers/CatsListContainer.js
@@ -10,7 +10,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        getCats: () => dispatch(getCats())
+        getCats: () => dispatch(getCats(dispatch))
     }
 };
 
--- src/js/ducks/CatsDuck.js
+++ src/js/ducks/CatsDuck.js
@@ -3,18 +3,40 @@ import keyMirror from 'keymirror';
 import cats_data from '../../data/cats.json';
 
 const default_state = {
-    cats: []
+    cats: [],
+    category: null
 };
 
 const Action_Types = keyMirror({
-    'GET_CATS': null
+    'GET_CATS': null,
+    'SET_CATS': null,
+    'SET_CAT_CATEGORY': null
 });
 
 export const getCats = createAction(
     Action_Types.GET_CATS,
-    () => {
+    (dispatch) => {
+        dispatch(setCats(dispatch, cats_data.cats));
+    }
+);
+
+export const setCats = createAction(
+    Action_Types.SET_CATS,
+    (dispatch, cats) => {
         return {
-            cats: cats_data.cats
+            cats
+        }
+    }
+);
+
+export const setCatCategory = createAction(
+    Action_Types.SET_CAT_CATEGORY,
+    (dispatch, cats, category) => {
+        const category_cats = cats.filter(cat => cat.category === category);
+        dispatch(setCats(dispatch, category_cats));
+
+        return {
+            category
         }
     }
 );
@@ -23,10 +45,16 @@ export default function reducer(state, action) {
     state = state || default_state;
 
     switch(action.type) {
-        case Action_Types.GET_CATS:
+        case Action_Types.SET_CATS:
             return {
                 ...state,
                 cats: action.payload.cats
+            };
+
+        case Action_Types.SET_CAT_CATEGORY:
+            return {
+                ...state,
+                category: action.payload.category
             };
 
         default:
